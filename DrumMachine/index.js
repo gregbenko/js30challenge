f4db10f07e6770e349017cd7425b512d01ed26ec
Gregory <gregory.benko.prieur@gmail.com>
2017-01-10 19:12:21 -0500

    30min to do all the HTML and CSS, now learning the JS part:)

--- DrumMachine/index.js
+++ DrumMachine/index.js
@@ -1,18 +1,16 @@
-function removeTransition(e) {
-  if (e.propertyName !== 'transform') return;
-  e.target.classList.remove('playing');
-}
-
-function playSound(e) {
+window.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-  if (!audio) return;
-
-  key.classList.add('playing');
-  audio.currentTime = 0;
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio)return; //stop function from running
+  audio.currentTime = 0; //rewind from start
   audio.play();
-}
+  key.classList.add('playing');
 
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+  function removeTransition(e) {
+    if(e.propertyName !=="transform") return;
+    this.classList.remove("playing");
+  };
+
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener("transitionend", removeTransition))
+});
