c1c54b7294dc73f0fd66920c7567d79deba14b5f
Gregory <gregory.benko.prieur@gmail.com>
2017-01-11 13:38:37 -0500

    Trying to understand flexboxto make it responsive and getting through the JS

--- DrumMachine/index.js
+++ DrumMachine/index.js
@@ -1,16 +1,20 @@
-window.addEventListener("keydown", function (e) {
+window.addEventListener("keydown", playSound);
+
+function playSound (e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-  if(!audio)return; //stop function from running
-  audio.currentTime = 0; //rewind from start
-  audio.play();
-  key.classList.add('playing');
+    if (!audio) {
+      console.log("Please play the right keys!");
+      return;
+    }
+    audio.play(); //play sample
+    audio.currentTime = 0; // rewind sample for multi key touch
+    key.classList.add('playing'); //adding the hit style when key pressed
+};
 
-  function removeTransition(e) {
-    if(e.propertyName !=="transform") return;
-    this.classList.remove("playing");
-  };
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 
-  const keys = document.querySelectorAll('.key');
-  keys.forEach(key => key.addEventListener("transitionend", removeTransition))
-});
+function removeTransition (e) {
+  this.classList.remove('playing');
+};
