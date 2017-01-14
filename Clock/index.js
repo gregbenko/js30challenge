ff0882f7ad29e6446062edd9aad666f9f9b56b69
Gregory <gregory.benko.prieur@gmail.com>
2017-01-14 12:45:57 -0500

    Repetition is the mother of skills

--- Clock/index.js
+++ Clock/index.js
@@ -1,22 +1,22 @@
 const hour = document.querySelector(".hour");
 const minute = document.querySelector(".minute");
-const seconds = document.querySelector(".seconds");
+const second = document.querySelector(".second");
 
 function setDate () {
-  const now = new Date();
+  const now = new Date;
 
-  const secondsTic = now.getSeconds();
-  const secondsTicDegrees = ((secondsTic / 60) * 360) + 90;
-  seconds.style.transform = `rotate(${secondsTicDegrees}deg)`;
+  const secondRotate = now.getSeconds();
+  const secondRotateDeg = ((secondRotate / 60) * 360);
+  second.style.transform = `rotate(${secondRotateDeg}deg)`;
 
-  const minuteTic = now.getMinutes();
-  const minuteTicDegrees = ((minuteTic / 60) * 360) + 90;
-  minute.style.transform = `rotate(${minuteTicDegrees}deg)`;
+  const minuteRotate = now.getMinutes();
+  const minuteRotateDeg = ((minuteRotate / 60) * 360) + (secondRotateDeg / 360);
+  minute.style.transform = `rotate(${minuteRotateDeg}deg)`;
 
-  const hourTic = now.getHours();
-  const hourTicDegrees = ((hourTic / 12) * 360) + 90;
-  hour.style.transform = `rotate(${hourTicDegrees}deg)`;
-};
+  const hourRotate = now.getHours();
+  const hourRotateDeg = ((hourRotate / 12) * 360) + (minuteRotateDeg / 12);
+  hour.style.transform = `rotate(${hourRotateDeg}deg)`;
+}
 
 setInterval(setDate, 1000);
 
